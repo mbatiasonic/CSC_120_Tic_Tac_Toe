148458e39f02490c96e870b5b1dd4de671fcd6a9
mbatiasonic <gachanasamuel@gmail.com>
2021-12-15 17:25:56 +0300

    Stage 2 of the game functionality added

--- Board.py
+++ Board.py
@@ -27,3 +27,16 @@ ___________________
         else:
             blankBoard = blankBoard.replace(str(i), ' ')
     print(blankBoard)
+def player_input():
+    player1 = input("Please pick a marker 'X' or 'O' ")
+    while True:
+        if player1.upper() == 'X':
+            player2='O'
+            print("You've choosen " + player1 + ". Player 2 will be " + player2)
+            return player1.upper(),player2
+        elif player1.upper() == 'O':
+            player2='X'
+            print("You've choosen " + player1 + ". Player 2 will be " + player2)
+            return player1.upper(),player2
+        else:
+             player1 = input("Please pick a marker 'X' or 'O' ")
