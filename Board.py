1c7cd7718fd8be2987da37921aef35453303699b
mbatiasonic <gachanasamuel@gmail.com>
2021-12-15 16:54:27 +0300

    Board Printing Functionality Added

---
+++ Board.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 16:29:03 2021
+
+@author: Gachana Samuel
+"""
+
+def display_board(board):
+    blankBoard="""
+___________________
+|     |     |     |
+|  7  |  8  |  9  |
+|     |     |     |
+|-----------------|
+|     |     |     |
+|  4  |  5  |  6  |
+|     |     |     |
+|-----------------|
+|     |     |     |
+|  1  |  2  |  3  |
+|     |     |     |
+|-----------------|
+"""
+    for i in range(1,10):
+        if (board[i] == 'O' or board[i] == 'X'):
+            blankBoard = blankBoard.replace(str(i), board[i])
+        else:
+            blankBoard = blankBoard.replace(str(i), ' ')
+    print(blankBoard)
